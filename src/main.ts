309154db30edef7e674ac44a1d4273aacbc39328
gufronnakaaw <gufronnakaaw@gmail.com>
2024-03-23 21:26:31 +0700

    add: export in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './utils/global.exception';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalFilters(new GlobalExceptionFilter(app.get(HttpAdapterHost)));
